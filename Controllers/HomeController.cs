ba5a1997431263707c6a218ae21330955fb5a621
bthompso11 <46456175+bthompso11@users.noreply.github.com>
2019-11-11 16:38:12 -0800

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FanSite.Models;
+using System.Linq;
+using System.Web;
+
+namespace FanSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        //Changed from ViewResult to IActionResult. --LAB 6 EDIT
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Created new controller and view to have controller return View --LAB6 EDIT
+        public String TestStringView()
+        {
+            return "From theTest String View";
+        }
+
+        //Created a new view for to Test this Action Result -- LAB6 EDIT
+       public ActionResult TestActionResultView()
+        {
+            return Redirect("Index");
+        }
+
+        //Created New View to reroute  -- LAB6 EDIT
+        public PartialViewResult TestPartialViewResult()
+        {
+            return PartialView("TestPartialView");
+        }
+
+        //Created new view and controller to test Content -- LAB6 EDIT
+        public ContentResult TestContentResult()
+        {
+            return Content("<h1>this is From the controller view</h1>", "text/html");
+        }
+        
+
+        [HttpGet]
+        public ViewResult Stories()
+        {
+            return View("Stories");
+        }
+
+        //Overloaded Stories 
+        [HttpPost]
+        public ViewResult Stories(UserStory userstories)
+        {
+            Repository.AddResponse(userstories);
+            Repository.SortStories();
+            return View("Submitted", userstories);
+        }
+
+        public ViewResult History()
+        {
+            return View("History");
+        }
+
+        public ViewResult Sources()
+        {
+            return View("Sources");
+        }
+
+        public ViewResult ListStories()
+        {
+            return View(Repository.stories);
+        }
+
+        public ViewResult BooksAndPrint()
+        {
+            Repository.SortBooksAndPrintList();
+            return View(Repository.printMedia);
+        }
+
+        public ViewResult AddComment()
+        {
+            ViewBag.Test = "Testing View Bag for excercise";
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult AddComment(string name, string comment)
+        {      
+            ViewBag.Test = "Testing View Bag for excercise";
+
+          
+            Repository.AddComment(new UserComments()
+            {
+                Name = name,
+                Comment = comment
+            });
+
+            Repository.AddCommentToStory(comment);           
+          
+            return RedirectToAction("Stories");
+        }
+
+      
+
+        
+
+        public ViewResult OnlineResources()
+        {
+            return View(Repository.onlineResources);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FanSite.Models
+{
+
+    
+    public static class Repository
+    {
+        public static List<UserComments> comments = new List<UserComments>();
+
+        public static UserComments testStory = new UserComments { Name = "Brian", Comment = "Test Comment 1" };
+
+        //Hard coded data for UserStories
+        public static List<UserStory> stories = new List<UserStory>
+        {
+            new UserStory{Name = "Brian", Location = "Springfield", Title = "first story", Story = "This is the first story",},                
+            new UserStory{Name = "Ashley", Location = "Eugene", Title = "Second story", Story = "This is the second story"}
+        };
+        
+        
+        //BooksAndPrint list is hardcoded at this time
+        public static List<BooksAndPrint> printMedia = new List<BooksAndPrint> {         
+            new BooksAndPrint{Title = "Secret History of Twin Peaks", Hyperlink = "http://www.bymarkfrost.com/books/secret_history_of_twin_peaks.html", PublicationYear = "2016"},
+            new BooksAndPrint{Title = "Twin Peaks Final Dossier", Hyperlink = "http://www.bymarkfrost.com/books/twin_peaks_final_dossier.html", PublicationYear = "2017"},
+            new BooksAndPrint{Title = "Secret Diary of Laura Palmer", Hyperlink = "http://www.bymarkfrost.com/books/secret_diary_of_laura_palmer.html", PublicationYear = "2011"}           
+            };             
+        
+        //OnlineResources list is hardcoded at this time.
+        public static List<OnlineResources> onlineResources = new List<OnlineResources> { 
+            new OnlineResources{Title = "Twin Peaks Wiki", Hyperlink = "https://en.wikipedia.org/wiki/Twin_Peaks"}      
+            };        
+
+        public static IEnumerable<UserStory> Stories
+        {
+            get
+            {
+                return stories;
+            }
+        }
+
+        //Adding responses to the list stories
+        public static void AddResponse(UserStory userStories)
+        {
+            stories.Add(userStories);
+        }
+
+        public static void AddCommentToStory(String userComment)
+        {
+            //TODO: Figure out logic to make this line work.
+            //stories[0].StoryComments = userComment;
+        }
+
+        //TODO: Need to put the comment object in all of the lists!!!! 
+
+      
+        public static IEnumerable<UserComments> Comment
+        {
+            get
+            {
+                return comments;
+            }
+        }
+
+        public static void AddComment(UserComments userComment)
+        {
+            comments.Add(userComment);
+        }
+
+        //Use of Lambda Expression for Lab3
+        public static IEnumerable<BooksAndPrint> PrintMedia { get => printMedia; }      
+        //Use if Lambda Expression for Lab3
+        public static IEnumerable<OnlineResources> OnlineResources { get => OnlineResources; }
+
+       public static void SortBooksAndPrintList()
+        {
+            printMedia.Sort((pm1, pm2) => string.Compare(pm1.PublicationYear, pm2.PublicationYear, StringComparison.Ordinal));
+        }
+
+        public static void SortStories()
+        {
+            stories.Sort((s1,s2) => string.Compare(s1.Title, s2.Title, StringComparison.Ordinal));
+        }
+
+
+    }
+}
